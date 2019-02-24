20871cb0193d180fea371d6fa2c6735578968d1d
sathi2 <sathi@wired2perform.com>
2019-02-24 15:08:39 +0530

    New service 10. jx import shoild run - 387bbe3f6257a6f6d93450261abf3f52de479c52

---
+++ src/main/java/com/example/springservice10/SpringService10Application.java
@@ -0,0 +1,13 @@
+package com.example.springservice10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringService10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringService10Application.class, args);
+	}
+
+}
